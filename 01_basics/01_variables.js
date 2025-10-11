65875357857fd98840a45bfa4f0bfb1628573062
Himanshu-K06 <himanshu952511@gmail.com>
2025-10-11 18:03:47 +0530

    add variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,7 @@
+const accuountId = 254615;
+let accountEmail = "himu.wtf69@gmail.com";
+let accountPassword = "huiHUIhui867";
+let accountState;
+let accountCity = "ghaziabad";
+// console.log() outputs only one thing, console.table([]) makes a table and oututs multiple things in a table manner 
+console.table([accuountId,accountEmail,accountPassword,accountCity,accountState]);
